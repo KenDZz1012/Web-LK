3cc18e2935323c5a2f8e1c9c7cb3b15e650a5847
kendz11012 <81511027+kendz11012@users.noreply.github.com>
2021-10-27 18:16:05 +0700

    Update www.js

--- src/bin/www.js
+++ src/bin/www.js
@@ -3,5 +3,5 @@ const app = require('../apps/app');
 const config = require('config');
 
 app.listen(port=config.get('app').port,()=>{
-    console.log("Server running on port"+port);
+        console.log(`server is running on port ${port}`);
 })
